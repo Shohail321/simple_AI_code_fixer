9670d9cd77b7132bc167135f82fbc78431d4d334
Shohail321 <shohailsarker321@gmail.com>
2026-04-19 18:42:05 -0400

    Ai code fixer initial stage

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from PIL import Image
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+from PIL import Image
+
+# Load API key
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+# Configure Gemini
+genai.configure(api_key=api_key)
+
+# Streamlit UI
+st.title("🤖 AI Code Debugger")
+st.divider()
+
+# File uploader
+uploaded_file = st.file_uploader(
+    "Upload your code error screenshot",
+    type=["png", "jpg", "jpeg"]
+)
+
+
+
+# Option bar
+option = st.selectbox(
+    "Choose Output Type",
+    ["Hints", "Solution with code"]
+)
+
+# Debug button
+debug_btn = st.button("Debug Code")
+
+# Button logic
+if debug_btn:
+
+    # Validation
+    if uploaded_file is None or option is None:
+        st.error("⚠️ Please upload an image and select an option!")
+    else:
+        image = Image.open(uploaded_file)
+
+        # Show spinner
+        with st.spinner("Analyzing your code... 🔍"):
+
+            # Gemini Model
+            model = genai.GenerativeModel("gemini-3-flash-preview")
+
+            # Prompt based on option
+            if option == "Hints":
+                prompt = """
+                Analyze this screenshot of code error.
+                Give ONLY hints to fix the issue.
+                Do NOT provide full solution code.
+                """
+            else:
+                prompt = """
+                Analyze this screenshot of code error.
+                Explain the error clearly and provide the correct fixed code step by step.
+                """
+
+            # Generate response
+            response = model.generate_content([prompt, image])
+
+            # Display result
+            st.markdown("## 🧠 Result")
+            st.markdown(response.text)
